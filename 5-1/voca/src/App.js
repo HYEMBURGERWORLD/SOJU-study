9395b19ce021ffd48b45e2f2b15f30a64f644c77
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-05-19 17:36:48 +0900

    🧑‍💻

--- 5-1/voca/src/App.js
+++ 5-1/voca/src/App.js
@@ -1,21 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+import styles from './App.module.css';
 
 function App() {
+  // const name = 'burger';
+  // const naver = {
+  //   name: 'naver',
+  //   url: 'https://naver.com',
+  // };
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>my first React !!</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1
+        style={{
+          color: 'red',
+          backgroundColor: 'green',
+        }}
+      >
+        Hello, {name}!!
+      </h1>
+      <a href={naver.url}>{naver.name}</a> */}
+      <Hello />
+      <Hello />
+      <Hello />
+      {/** 중간에 내용이 없으면 셀프클로징 추천 */}
+      {/* <Welcome /> */}
+      {/* <div className={styles.box}>App</div> */}
     </div>
   );
 }
